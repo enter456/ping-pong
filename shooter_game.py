0f989fb326f36f71497610b9d928ab6991cef517
enter456 <118670168+enter456@users.noreply.github.com>
2022-11-20 17:30:27 +0300

    Update shooter_game.py
    добавился класс GameSprite

--- shooter_game.py
+++ shooter_game.py
@@ -4,8 +4,16 @@ win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
-
-
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        sprite.Sprite.__init__(self)
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 clock = time.Clock()
 FPS = 60
